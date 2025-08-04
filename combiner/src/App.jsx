4fd139cbf5e041fa1380737697309d6e29750a4d
sybrk <snn.yll@gmail.com>
2025-08-04 22:22:17 +0300

    using object and ids when importing

--- combiner/src/App.jsx
+++ combiner/src/App.jsx
@@ -1,32 +1,45 @@
 import { useState } from 'react'
 
 import './App.css'
-import { readFile, xmlParser } from './utils/Util'
+import { idGenerator, readFile, xmlParser } from './utils/Util'
 
 function App() {
 
-  const [regexList, setRegexList] = useState([])
+  const [regexList, setRegexList] = useState({})
 
-  const importRegex = async() => {
+  const importRegex = async () => {
+    const tmpObj = { ...regexList }
     const files = document.getElementById("regexfiles").files
     for (let index = 0; index < files.length; index++) {
       const file = files[index];
       const fileRead = await readFile(file);
       const parseFile = xmlParser(fileRead.fileContent)
+      const fileId = idGenerator();
+      tmpObj[fileId] = {}
+      tmpObj[fileId]["name"] = fileRead.fileName;
+      tmpObj[fileId]["original"] = fileRead.fileContent;
+      tmpObj[fileId]["regexes"] = {}
       let regexRules = Array.from(parseFile.querySelectorAll("RegExRule"));
       console.log("regexrules", regexRules)
       regexRules = regexRules.filter(x => /RegExRules\d+$/.test(x.parentElement.getAttribute('Id')))
-      const regexes = regexRules.map(x => {
-        return {
-          description: x.querySelector("Description").textContent,
-          ignoreCase: x.querySelector("IgnoreCase").textContent,
-          source: x.querySelector("RegExSource").textContent,
-          target: x.querySelector("RegExTarget").textContent,
-          condition: x.querySelector("RuleCondition").textContent,
-        }
+
+      regexRules.map((x, i) => {
+        tmpObj[fileId]["regexes"][i] = {}
+        tmpObj[fileId]["regexes"][i]["description"] = x.querySelector("Description").textContent;
+        tmpObj[fileId]["regexes"][i]["ignoreCase"] = x.querySelector("IgnoreCase").textContent;
+        tmpObj[fileId]["regexes"][i]["source"] = x.querySelector("RegExSource").textContent;
+        tmpObj[fileId]["regexes"][i]["target"] = x.querySelector("RegExTarget").textContent;
+        tmpObj[fileId]["regexes"][i]["condition"] = x.querySelector("RuleCondition").textContent;
       });
-      setRegexList(prev => [...prev, ...regexes])
+
     }
+    setRegexList(tmpObj)
+  }
+  const handleChanges = (e) => {
+    const data = e.target.dataset;
+    const tmpObj = {...regexList}
+    tmpObj[data.fileId]["regexes"][data.regexId][data.field] = e.target.value;
+    setRegexList(tmpObj)
   }
   return (
     <>
@@ -35,6 +48,7 @@ function App() {
       <table>
         <thead>
           <tr>
+            <th>File</th>
             <th>Description</th>
             <th>IgnoreCase</th>
             <th>Source</th>
@@ -43,17 +57,28 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {regexList
-            ? regexList.map((regex, i) => {
+          {Object.keys(regexList)
+            ? Object.keys(regexList).map((file, i) => {
               return (
-                <tr key={"regex" + i}>
-                  <td>{regex?.description}</td>
-                  <td>{regex?.ignoreCase}</td>
-                  <td>{regex?.source}</td>
-                  <td>{regex?.target}</td>
-                  <td>{regex?.condition}</td>
-                </tr>
-              );
+                <>
+                  {
+                    Object.keys(regexList[file]["regexes"]).map((regex, j) => {
+                      return (
+                        <tr key={"regex" + j} data-file-id={file} data-regex-id={j}>
+                          <td>{regexList[file]?.name}</td>
+                          <td><textarea data-file-id={file} data-regex-id={j} data-field="description" onChange={handleChanges} type="text" name="" id="" value={regexList[file]["regexes"][regex]?.description} /></td>
+                          <td>{regexList[file]["regexes"][regex]?.ignoreCase}</td>
+                          <td>{regexList[file]["regexes"][regex]?.source}</td>
+                          <td>{regexList[file]["regexes"][regex]?.target}</td>
+                          <td>{regexList[file]["regexes"][regex]?.condition}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </>
+              )
+
+
             })
             : null}
         </tbody>
--- combiner/src/utils/Util.jsx
+++ combiner/src/utils/Util.jsx
@@ -1,19 +1,27 @@
 export const xmlParser = (xmlContent) => {
-    let parser = new DOMParser();
-    let xmldoc = parser.parseFromString(xmlContent, "text/xml");
-    return xmldoc;
+  let parser = new DOMParser();
+  let xmldoc = parser.parseFromString(xmlContent, "text/xml");
+  return xmldoc;
 }
 
 export const readFile = async (file) => {
-    let reader = new FileReader();
-    // read file as text
-    reader.readAsText(file);
-    // await the file to be read.
-    await new Promise((resolve) => (reader.onload = () => resolve()));
-  
-    // return filename and filecontent with keys.
-    return {
-      fileName: file.name,
-      fileContent: reader.result,
-    };
+  let reader = new FileReader();
+  // read file as text
+  reader.readAsText(file);
+  // await the file to be read.
+  await new Promise((resolve) => (reader.onload = () => resolve()));
+
+  // return filename and filecontent with keys.
+  return {
+    fileName: file.name,
+    fileContent: reader.result,
   };
+};
+export const idGenerator = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
