5283401468d33d5babc31501c776e460fc49377c
sybrk <snn.yll@gmail.com>
2025-08-23 19:31:29 +0300

    testing mock

--- combiner/src/App.jsx
+++ combiner/src/App.jsx
@@ -4,9 +4,7 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Combiner from './Combiner'
 import HeroCustomized from './components/HeroCustomized'
-import TradosRegexHero from './components/HeroV1'
 import Navbar from './components/navigation/Navbar'
-import Features from './components/hero/features/Features'
 import Support from './components/support/Support'
 import HeroV1 from './components/HeroV1'
 
@@ -25,7 +23,7 @@ function App() {
     
     <Navbar />
       <Routes>
-        <Route index element={<HeroCustomized />} />
+        <Route index element={<HeroV1 />} />
         <Route path="combiner" element={<Combiner />} />
         <Route path="support" element={<Support />} />
       </Routes>
--- combiner/src/components/navigation/Navbar.jsx
+++ combiner/src/components/navigation/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                         </div>
                         <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             
-                        <li><NavLink to="/features" end>
+                        <li><NavLink to="/combiner" end>
                             Combiner
                         </NavLink></li>
                             <li><a href="./support">
