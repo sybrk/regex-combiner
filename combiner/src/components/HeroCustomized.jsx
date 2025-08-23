a14ef6550ccec51470bfb88563737d82e6b3b3f4
sybrk <snn.yll@gmail.com>
2025-08-23 15:20:25 +0300

    tailwind fix again

--- combiner/src/components/HeroCustomized.jsx
+++ combiner/src/components/HeroCustomized.jsx
@@ -45,7 +45,8 @@ const HeroCustomized = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
-      <div className="opacity-0 opacity-100 translate-y-0 translate-y-12" style={{ display: "none" }} />
+      <div className="opacity-0 translate-y-0" />
+      <div className="opacity-100 translate-y-12" />
 
       {/* Navbar */}
       
