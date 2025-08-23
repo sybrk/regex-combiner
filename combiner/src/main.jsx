051b890778a5f1a0e9f59019f5548e729fda0a9e
sybrk <snn.yll@gmail.com>
2025-08-23 14:47:48 +0300

    basename update

--- combiner/src/main.jsx
+++ combiner/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/regex-combiner">
+      <BrowserRouter basename="/regex-combiner/">
         <App />
       </BrowserRouter>
     </Provider>
