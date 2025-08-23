5dff5d2b2698053ba0fac04934c5b9dc4a8da894
sybrk <snn.yll@gmail.com>
2025-08-23 14:14:09 +0300

    route and base update

--- combiner/src/main.jsx
+++ combiner/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/regex-combiner">
         <App />
       </BrowserRouter>
     </Provider>
