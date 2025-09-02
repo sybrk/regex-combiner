fdd827c01370b90d6588a76bb2da63eff0464067
Sinan Yolal <snn.yll@gmail.com>
2025-09-02 18:15:35 +0000

    validating with message

--- blazor/BlazorRegexValidator/RegexValidator.cs
+++ blazor/BlazorRegexValidator/RegexValidator.cs
@@ -1,8 +1,29 @@
 using System.Text.RegularExpressions;
 using Microsoft.JSInterop;
 
+public class RegexResult
+{
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+}
 public static class RegexValidator
 {
+    [JSInvokable("ValidateRegexDetailed")]
+    public static RegexResult MatchRegexDetailed(string pattern)
+    {
+        try
+        {
+            _ = Regex.Match("Hello world", pattern);
+            return new RegexResult { Success = true, Error = null };
+        }
+        catch (System.Exception ex)
+        {
+            return new RegexResult { Success = false, Error = ex.Message };
+        }
+
+
+    }
+
     [JSInvokable("ValidateRegex")]
     public static bool MatchRegex(string pattern)
     {
@@ -16,7 +37,5 @@ public static class RegexValidator
 
             return false;
         }
-        
-        
     }
 }
